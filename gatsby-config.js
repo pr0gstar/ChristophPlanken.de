a9148e40e2a03bec444f112c47899dfdcb1c2eb6
pr0gstar <c.planken@absturzsicherung.de>
2021-03-24 07:11:40 +0100

    added packages and changed information in package json and gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `ChristophPlanken.de`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Christoph Planken`,
+    description: `Christophs persönliche Webseite!`,
+    author: `Christoph Planken <me@christophplanken.de>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
